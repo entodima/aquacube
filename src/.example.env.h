68fa611e29da0f6c86a48b679bb1ff383f2bc111
Dmitry Parkhomenko <entodima@gmail.com>
2020-10-29 11:29:20 +0300

    Добавлен файл env

---
+++ src/.example.env.h
@@ -0,0 +1,6 @@
+#define SSID "YOUR_NAME_OF_WIFI"
+#define SSID_PASSWORD "WIFI_PASSWORD"
+#define BOT_TOKEN "TELEGRAM_BOT_TOKEN_FROM_BOT_FATHER"
+
+#define USERS {0000000}    // allowed chatid
+#define ADMINS = 1          // first N chatid's is admins
